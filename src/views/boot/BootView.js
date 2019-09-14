a39c0ca20a55a8ebff9e2408660102914a415bba
tonyduanesmith <tonyduanesmith@me.com>
2019-09-14 19:23:18 +0100

    Added appicon svg and styled boot page

--- src/views/boot/BootView.js
+++ src/views/boot/BootView.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import styled from 'styled-components'
+import { View, Image } from 'react-native'
+import AppIcon from '../../assets/icons/AppIcon'
+
+const StyledView = styled.View`
+	flex: 1;
+	background-color: #029cf5;
+	justify-content: center;
+	align-items: center;
+`
 
 const BootView = () => {
 	return (
-		<View>
-			<Text>Boot View</Text>
-		</View>
+		<StyledView>
+			<AppIcon />
+		</StyledView>
 	)
 }
 
