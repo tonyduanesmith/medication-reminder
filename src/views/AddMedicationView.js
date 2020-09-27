3155cd83d777c13c635c36221661f6b91a77f97d
Anthony Smith <anthonysmith@anthonys-imac.lan>
2020-09-27 17:22:34 +0100

    fix: Add reminder framework

--- src/views/AddMedicationView.js
+++ src/views/AddMedicationView.js
@@ -1,32 +1,33 @@
-import React, { useContext } from 'react'
-import { Context as MedicationContext } from '../context/MedicationContext'
-import MedicationDetails from '../components/MedicationDetails'
-import { BlurView } from 'expo-blur'
+import React, { useContext } from "react";
+import { BlurView } from "expo-blur";
+
+import { Context as MedicationContext } from "../context/MedicationContext";
+import MedicationDetails from "../components/MedicationDetails";
 
 const AddMedicationView = () => {
-	const {
-		state: { medications },
-		addMedication
-	} = useContext(MedicationContext)
+    const {
+        state: { medications },
+        addMedication,
+    } = useContext(MedicationContext);
 
-	return (
-		<MedicationDetails
-			title='Add Medication'
-			onSave={(name, doseSize, colour, count) =>
-				addMedication(name, doseSize, colour, count, medications)
-			}
-		/>
-	)
-}
+    return (
+        <MedicationDetails
+            title="Add Medication"
+            onSave={(name, doseSize, colour, count) =>
+                addMedication(name, doseSize, colour, count, medications)
+            }
+        />
+    );
+};
 
 AddMedicationView.navigationOptions = {
-	headerStyle: {
-		borderBottomWidth: 0
-	},
-	headerBackground: (
-		<BlurView style={{ flex: 1 }} tint='light' intensity={90} />
-	),
-	headerTransparent: true
-}
+    headerStyle: {
+        borderBottomWidth: 0,
+    },
+    headerBackground: (
+        <BlurView style={{ flex: 1 }} tint="light" intensity={90} />
+    ),
+    headerTransparent: true,
+};
 
-export default AddMedicationView
+export default AddMedicationView;
--- src/views/AddReminderView.js
+++ src/views/AddReminderView.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { BlurView } from "expo-blur";
+
+import ReminderDetails from "../components/ReminderDetails";
 
 const AddReminderView = () => {
-	return (
-		<View>
-			<Text>Add Reminder View</Text>
-		</View>
-	)
-}
+    return <ReminderDetails title="Add Reminder" />;
+};
 
-export default AddReminderView
+AddReminderView.navigationOptions = {
+    headerStyle: {
+        borderBottomWidth: 0,
+    },
+    headerBackground: (
+        <BlurView style={{ flex: 1 }} tint="light" intensity={90} />
+    ),
+    headerTransparent: true,
+};
+
+export default AddReminderView;
